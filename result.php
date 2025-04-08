35055644d213c99d253ca652f85070df36914729
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-04-08 15:03:05 +0000

    This is my code in php

---
+++ result.php
@@ -0,0 +1,11 @@
+   <?php
+	// get the base and height from the textfields
+	$sideA = $_POST['sideA'];
+    $sideB = $_POST['sideB'];
+    $height = $_POST['height'];
+
+	// This is to calculate the area
+	$area = ($sideA + $sideB)/2*$height 
+?>
+<h3>Results:</h3>
+The area of the Trapezoid is <?php echo "$area" ?>cm<sup>2</sup>.
