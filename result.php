80272fca3ac6d387a9ec2dd8812cec77b413e195
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-04-09 14:30:13 +0000

    the old code did not work i hope this dose

--- result.php
+++ result.php
@@ -6,8 +6,7 @@
 
 	// This is to calculate the area
 	$area = ($sideA + $sideB)/2*$height 
-	?>
-	<?php
+
     echo "<h3>Results:</h3>
 The area of the Trapezoid is <?php echo "$area" ?>cm<sup>2</sup>."
 ?>
